2fa729cc0acdca5f26ddc15d3680f62af1b87c54
Matias Pereyra <122374760+pereyramatias@users.noreply.github.com>
2025-05-05 18:22:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
+
 const authRoutes = require('./routes/auth');
 const facturaRoutes = require('./routes/facturas');
 const { authMiddleware } = require('./middleware/auth');
@@ -11,6 +11,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Agregá esta línea 👇
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
 
 app.use('/api/auth', authRoutes);
 app.use('/api/facturas', authMiddleware, facturaRoutes);
